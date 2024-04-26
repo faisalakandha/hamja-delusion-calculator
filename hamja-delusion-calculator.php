69e36ac502409079bd07537eaab78bee8fc25eea
Faisal Akandha <faisalakandha24@gmail.com>
2024-04-26 09:42:05 +0000

    plugin activation-deactivation hooks added

--- hamja-delusion-calculator.php
+++ hamja-delusion-calculator.php
@@ -9,3 +9,26 @@
  * Author URI: https://faisal.github.io/
  * License: GPL2
  **/
+
+
+ function my_plugin_activate()
+{
+
+}
+
+register_activation_hook(__FILE__, 'my_plugin_activate');
+
+
+
+function load_plugin()
+{
+}
+
+add_action('admin_init', 'load_plugin');
+
+
+function my_deactivation()
+{
+}
+
+register_deactivation_hook(__FILE__, 'my_deactivation');
