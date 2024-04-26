ada5a44f668dda130171750336f2938190f97219
Faisal Akandha <faisalakandha24@gmail.com>
2024-04-26 09:38:36 +0000

    plugin init

---
+++ hamja-delusion-calculator.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Plugin Name: Hamja Delusion Calculator
+ * Plugin URI: https://delusionmeter.com/
+ * Description: A plugin for Content Recommendation
+ * Version: 1.0.0
+ * Author: H.A.B.M. Faisal Akandha
+ * Author URI: https://faisal.github.io/
+ * License: GPL2
+ **/
