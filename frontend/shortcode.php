b6fb6b65ad90f3988fdbdcb6adf3d8398d233e74
Faisal Akandha <57830958+faisalakandha@users.noreply.github.com>
2024-05-02 13:20:20 +0600

    Update shortcode.php

--- frontend/shortcode.php
+++ frontend/shortcode.php
@@ -125,9 +125,7 @@ function DelusionCalculator()
       </div>
       <div class="py-6">
         <p class="font-medium mb-2">Minimum Income</p>
-        <input
-          class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
-          placeholder="$0" />
+        <input class="w-full h-10" type="range" min="1" max="100" value="50">
       </div>
       <div>
         <p class="font-medium mb-2">Ethnicity</p>
