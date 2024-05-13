443098498e4eb059b8cae965c2f31777b7df8cbe
Faisal Akandha <57830958+faisalakandha@users.noreply.github.com>
2024-05-14 01:49:56 +0600

    Update shortcode.php

--- frontend/shortcode.php
+++ frontend/shortcode.php
@@ -5,18 +5,6 @@ require_once (plugin_dir_path(__FILE__) . '/calculator.php');
 function DelusionCalculator()
 {
 
-  $test_data = [
-    'age_range' => [25, 35],  // Age range between 25 and 35
-    'income_slider' => 75000,   // Minimum income of $75,000
-    'race' => 'White',  // Selected races: White and Asian
-    'height_feet' => 5,
-    'height_inches' => 11,
-    'gender_preference' => 'Male', // Optional: Male partner preference
-    'exclude_obese' => true,    // Optional: Exclude obese individuals
-  ];
-
-  $results = calculate_dating_pool($test_data);
-
   ob_start(); ?>
 
 
@@ -29,7 +17,7 @@ function DelusionCalculator()
       integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
       crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet"
-      href="http://wp.docker.localhost:8000/wp-content/plugins/hamja-delusion-calculator/frontend/range.css" />
+      href="https://delusionmeter.com//wp-content/plugins/hamja-delusion-calculator/frontend/range.css" />
 
     <style>
       /* Style for the thumbs */
@@ -52,10 +40,9 @@ function DelusionCalculator()
 
 
       .selected {
-        background-color: black;
-        /* or any other desired background color */
-        color: white;
-        /* adjust text color if needed */
+        background-color: white;
+        color: black;
+        padding: 5px 20px 5px 20px;
       }
 
 
@@ -74,52 +61,67 @@ function DelusionCalculator()
       #ethnicity-options li:hover {
         background-color: #f3f4f6;
       }
+
+      .align-center {
+        align-items: center;
+      }
+
+      .gender-label {
+        display: flex;
+        align-items: center;
+        /* Aligns content vertically */
+      }
+
+      .gender-radio {
+        margin-right: 5px;
+        /* Adjust margin as needed */
+      }
+
     </style>
 
   </head>
 
   <body>
 
-    <form action="/test-form-data/" method="POST" class="comment-form">
+    <form action="/delusion-calculator-result/" method="POST" class="comment-form">
 
-      <div style="box-shadow: 0px 20px 20px 0px lightblue;" class="bg-white p-8 rounded-lg shadow max-w-md mx-auto">
-        <h2 class="text-2xl font-semibold mb-6">What Percentage of People in United States meet your standards?</h2>
+      <div style="" class="bg-white p-8 rounded-lg shadow max-w-md mx-auto">
+        <div class="text-4xl font-semibold mb-6 text-black">What Percentage of People in United States meet your standards?
+        </div>
         <div class="space-y-6">
-          <div>
-            <p class="font-medium mb-2">Gender</p>
-            <div class="flex gap-2">
-              <input type="button" id="men" name="men"
-                class="inline-flex gender-button hover:bg-black hover:text-white border border-input items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 flex-1 cursor-pointer"
-                value="Men" Men />
-              <input type="button" id="women" name="women"
-                class="inline-flex gender-button hover:bg-black hover:text-white items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 flex-1 cursor-pointer"
-                value="Women" Women />
+          <div class="flex items-center">
+            <div class="font-medium mb-2 font-bold">Gender</div>
+            <div style="border-radius:5px;" class="flex gap-x-8 align-center bg-[#F4F4F4] ml-auto px-6 py-2">
+              <button style="border-radius:5px;" type="button" id="women"
+                class="gender-button text-[#766C71] hover:text-black selected">
+                Men
+              </button>
+              <button type="button" id="men" class="gender-button text-[#766C71] hover:text-black">
+                Women
+              </button>
+              <input type="hidden" id="selectedGender" name="gender" value="men">
             </div>
+
           </div>
 
-          <div class="pb-6">
+          <div class="py-2">
             <div class="font-medium mb-2 flex">
-              <p>Age:</p>
-              <span id="leftThumbValue" style="margin-left:auto; font-weight:bold;">30</span>
-              <span>-</span>
-              <span id="rightThumbValue" style="font-weight:bold;">60</span>
-            </div>
-
-            <div slider id="slider-distance">
-              <div>
-                <div inverse-left style="width:70%;"></div>
-                <div inverse-right style="width:70%;"></div>
-                <div range style="left:30%;right:40%;"></div>
-                <span thumb style="left:30%;"></span>
-                <span thumb style="left:60%;"></span>
-                <div sign style="left:30%;">
-                  <span id="value">30</span>
+              <p class="font-bold">Age</p>
+              <div style="margin-left:auto; margin-right:30px; width: 250px;" slider id="slider-distance">
+                <div>
+                  <div inverse-left style="width:70%;"></div>
+                  <div inverse-right style="width:70%;"></div>
+                  <div range style="left:30%;right:40%;"></div>
+                  <span thumb style="left:30%;"></span>
+                  <span thumb style="left:60%;"></span>
+                  <div sign style="left:30%;">
+                    <span id="value">30</span>
+                  </div>
+                  <div sign style="left:60%;">
+                    <span id="value">60</span>
+                  </div>
                 </div>
-                <div sign style="left:60%;">
-                  <span id="value">60</span>
-                </div>
-              </div>
-              <input type="range" name="ageMin" tabindex="0" value="30" max="100" min="0" step="1" oninput="
+                <input type="range" name="ageMin" tabindex="0" value="30" max="100" min="0" step="1" oninput="
   this.value=Math.min(this.value,this.parentNode.childNodes[5].value-1);
   var value=(100/(parseInt(this.max)-parseInt(this.min)))*parseInt(this.value)-(100/(parseInt(this.max)-parseInt(this.min)))*parseInt(this.min);
   var children = this.parentNode.childNodes[1].childNodes;
@@ -128,7 +130,7 @@ function DelusionCalculator()
   children[7].style.left=value+'%';children[11].style.left=value+'%';
   children[11].childNodes[1].innerHTML=this.value;" />
 
-              <input type="range" name="ageMax" tabindex="0" value="60" max="100" min="0" step="1" oninput="
+                <input type="range" name="ageMax" tabindex="0" value="60" max="100" min="0" step="1" oninput="
   this.value=Math.max(this.value,this.parentNode.childNodes[3].value-(-1));
   var value=(100/(parseInt(this.max)-parseInt(this.min)))*parseInt(this.value)-(100/(parseInt(this.max)-parseInt(this.min)))*parseInt(this.min);
   var children = this.parentNode.childNodes[1].childNodes;
@@ -136,43 +138,48 @@ function DelusionCalculator()
   children[5].style.right=(100-value)+'%';
   children[9].style.left=value+'%';children[13].style.left=value+'%';
   children[13].childNodes[1].innerHTML=this.value;" />
+              </div>
+              <div>
+                <span id="leftThumbValue" style="font-weight:bold;">30</span>
+                <span>-</span>
+                <span id="rightThumbValue" style="font-weight:bold;">60</span>
+              </div>
             </div>
+
+
           </div>
         </div>
 
-        <fieldset class="flex my-8">
+        <div class="flex pb-6">
           <div>
-            <label for="obese"><b>Exclude Obese</b></label>
-            <input type="checkbox" name="obese" id="obese" />
+            <label class="text-[#FF1D74]" for="obese"><b>Exclude Obese</b></label>
+            <input type="checkbox" class="accent-pink-500" name="obese" id="obese" />
           </div>
           <div style="margin-left:auto;">
-            <label style="" for="married"><b>Exclude Married</b></label>
-            <input type="checkbox" name="married" id="married" />
+            <label class="text-[#FF1D74]" style="" for="married"><b>Exclude Married</b></label>
+            <input type="checkbox" class="accent-pink-500" name="married" id="married" />
           </div>
-        </fieldset>
+        </div>
 
         <div class="pb-6">
-          <div class="font-medium mb-2 flex">
-            <p>Height:</p>
-            <span id="leftThumbValueH" style="margin-left:auto; font-weight:bold;">5'0''</span>
-            <span>-</span>
-            <span id="rightThumbValueH" style="font-weight:bold;">6'0''</span>
-          </div>
-          <div slider id="slider-height">
-            <div>
-              <div inverse-left style="width:70%;"></div>
-              <div inverse-right style="width:70%;"></div>
-              <div range style="left:30%;right:40%;"></div>
-              <span class="thumb drop-shadow-xl" id="leftThumb" style="left:30%;" data-feet="5" data-inches="0"></span>
-              <span class="thumb drop-shadow-xl" id="rightThumb" style="left:60%;" data-feet="6" data-inches="0"></span>
-              <div sign style="left:30%;">
-                <span id="leftThumbFeet">5</span>'<span id="leftThumbInches">0</span>"
+          <div class="font-medium flex">
+            <p class="font-bold" style="width:15%">Height</p>
+            <div style="width:55%;margin-left:auto; margin-right:5px; width:200px; position:relative; margin-left:auto;" slider
+              id="slider-height">
+              <div>
+                <div inverse-left style="width:70%;"></div>
+                <div inverse-right style="width:70%;"></div>
+                <div range style="left:30%;right:40%;"></div>
+                <span class="thumb drop-shadow-xl" id="leftThumb" style="left:30%;" data-feet="5" data-inches="0"></span>
+                <span class="thumb drop-shadow-xl" id="rightThumb" style="left:60%;" data-feet="6" data-inches="0"></span>
+                <div sign style="left:30%;">
+                  <span id="leftThumbFeet">2</span>'<span id="leftThumbInches">6</span>"
+                </div>
+                <div sign style="left:60%;">
+                  <span id="rightThumbFeet">5</span>'<span id="rightThumbInches">0</span>"
+                </div>
               </div>
-              <div sign style="left:60%;">
-                <span id="rightThumbFeet">6</span>'<span id="rightThumbInches">0</span>"
-              </div>
-            </div>
-            <input name="heightMin" type="range" tabindex="0" value="30" max="100" min="0" step="1" oninput="
+              <input name="heightMin" type="range" tabindex="0" value="30" max="100" min="0" step="1" oninput="
       this.value=Math.min(this.value,this.parentNode.childNodes[5].value-1);
       var value=(100/(parseInt(this.max)-parseInt(this.min)))*parseInt(this.value)-(100/(parseInt(this.max)-parseInt(this.min)))*parseInt(this.min);
       var children = this.parentNode.childNodes[1].childNodes;
@@ -186,7 +193,7 @@ function DelusionCalculator()
 
     " />
 
-            <input name="heightMax" type="range" tabindex="0" value="60" max="100" min="0" step="1" oninput="
+              <input name="heightMax" type="range" tabindex="0" value="60" max="100" min="0" step="1" oninput="
       this.value=Math.max(this.value,this.parentNode.childNodes[3].value-(-1));
       var value=(100/(parseInt(this.max)-parseInt(this.min)))*parseInt(this.value)-(100/(parseInt(this.max)-parseInt(this.min)))*parseInt(this.min);
       var children = this.parentNode.childNodes[1].childNodes;
@@ -198,34 +205,35 @@ function DelusionCalculator()
       document.getElementById('rightThumb').setAttribute('data-inches', this.value % 12);
       document.getElementById('rightThumbFeet').textContent = `${Math.floor(this.value / 12)}'${this.value % 12}''`;
     " />
+            </div>
+            <div style="width:30%; margin-left:auto;">
+              <span id="leftThumbValueH" style="margin-left:auto; font-weight:bold;">2'6''</span>
+              <span>-</span>
+              <span id="rightThumbValueH" style="font-weight:bold;">5'0''</span>
+            </div>
           </div>
+
         </div>
 
 
-        <div class="pb-6">
-          <div class="font-medium mb-2 flex py-6">
-            <p>Income:</p>
-            <span style="margin-left:auto;">$</span><span id="leftThumbValueI"
-              style="font-weight:bold;">30</span><span>K</span>
-            <span> - </span>
-            <span>$</span><span id="rightThumbValueI" style="font-weight:bold;">60</span><span>K</span>
-          </div>
-
-          <div slider id="slider-distance">
-            <div>
-              <div inverse-left style="width:70%;"></div>
-              <div inverse-right style="width:70%;"></div>
-              <div range style="left:30%;right:40%;"></div>
-              <span thumb style="left:30%;"></span>
-              <span thumb style="left:60%;"></span>
-              <div sign style="left:30%;">
-                <span id="value">30</span>
+        <div class="pb-4">
+          <div class="font-medium mb-2 flex">
+            <p class="font-bold">Income</p>
+            <div style="width:200px" slider id="slider-distance">
+              <div>
+                <div inverse-left style="width:70%;"></div>
+                <div inverse-right style="width:70%;"></div>
+                <div range style="left:30%;right:40%;"></div>
+                <span thumb style="left:30%;"></span>
+                <span thumb style="left:60%;"></span>
+                <div sign style="left:30%;">
+                  <span id="value">30</span>
+                </div>
+                <div sign style="left:60%;">
+                  <span id="value">60</span>
+                </div>
               </div>
-              <div sign style="left:60%;">
-                <span id="value">60</span>
-              </div>
-            </div>
-            <input name="minIncome" type="range" tabindex="0" value="30" max="100" min="0" step="1" oninput="
+              <input name="minIncome" type="range" tabindex="0" value="30" max="100" min="0" step="1" oninput="
   this.value=Math.min(this.value,this.parentNode.childNodes[5].value-1);
   var value=(100/(parseInt(this.max)-parseInt(this.min)))*parseInt(this.value)-(100/(parseInt(this.max)-parseInt(this.min)))*parseInt(this.min);
   var children = this.parentNode.childNodes[1].childNodes;
@@ -236,7 +244,7 @@ function DelusionCalculator()
   document.getElementById('leftThumbValueI').textContent = `${this.value * 5}`;
   " />
 
-            <input name="maxIncome" type="range" tabindex="0" value="60" max="100" min="0" step="1" oninput="
+              <input name="maxIncome" type="range" tabindex="0" value="60" max="100" min="0" step="1" oninput="
   this.value=Math.max(this.value,this.parentNode.childNodes[3].value-(-1));
   var value=(100/(parseInt(this.max)-parseInt(this.min)))*parseInt(this.value)-(100/(parseInt(this.max)-parseInt(this.min)))*parseInt(this.min);
   var children = this.parentNode.childNodes[1].childNodes;
@@ -247,23 +255,33 @@ function DelusionCalculator()
   document.getElementById('rightThumbValueI').textContent = `${this.value * 5}`;
 
   " />
+            </div>
+            <div>
+              <span style="margin-left:auto;">$</span><span id="leftThumbValueI"
+                style="font-weight:bold;">30</span><span>K</span>
+              <span> - </span>
+              <span>$</span><span id="rightThumbValueI" style="font-weight:bold;">60</span><span>K</span>
+            </div>
           </div>
+
         </div>
         <div>
-          <p class="font-medium mb-2">Ethnicity</p>
-          <div class="relative">
-            <select name="ethnicity" id="ethnicity">
-              <option value="white">White</option>
-              <option value="black">Black</option>
-              <option value="hispanic">Hispanic</option>
-              <option value="asian">Asian</option>
-              <option value="american-indian-alaska-native">American Indian/Alaska Native</option>
-              <option value="other-multiple">Other/Multiple</option>
-            </select>
+          <div class="flex">
+            <div class="font-medium mb-2 font-bold">Ethnicity</div>
+            <div class="relative ml-auto">
+              <select style="border: 2px solid #FF1D74;" name="ethnicity" id="ethnicity">
+                <option value="white">White</option>
+                <option value="black">Black</option>
+                <option value="hispanic">Hispanic</option>
+                <option value="asian">Asian</option>
+                <option value="american-indian-alaska-native">American Indian/Alaska Native</option>
+                <option value="other-multiple">Other/Multiple</option>
+              </select>
+            </div>
           </div>
-          <div class="mt-6">
+          <div class="mt-12">
             <button type="submit"
-              class="inline-flex justify-between hover:bg-blue-400 text-xl py-8 bg-black text-white items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 w-full py-3">
+              class="inline-flex justify-between hover:bg-[#FF1D74] text-xl py-8 bg-black text-white items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 w-full py-3">
               Let's Find Out <i class="fa-regular fa-circle-right"></i>
             </button>
           </div>
@@ -330,9 +348,11 @@ function DelusionCalculator()
       // Add click event listener to each gender button
       genderButtons.forEach(button => {
         button.addEventListener('click', function () {
+          // Set the value of the hidden input to the clicked button's value
+          document.getElementById('selectedGender').value = this.id;
+
           // Check if the button is already selected
           const isSelected = this.classList.contains('selected');
-          console.log("Button Clicked ! Gender !");
           // If it's not selected, deselect all buttons and select the clicked one
           if (!isSelected) {
             // Reset background color for all buttons
@@ -347,6 +367,7 @@ function DelusionCalculator()
           }
         });
       });
+
 
     </script>
 
